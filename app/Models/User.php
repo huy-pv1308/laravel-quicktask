8a6a60ac13e90400cefac22b8c07fd3f867b940a
Pham Vu Huy <pham.vu.huy@sun-asterisk.com>
2022-11-24 13:55:30 +0700

    Add seeder and factory for user and ticket
    update tokenfactory

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -56,7 +57,7 @@ class User extends Authenticatable
     }
 
     public function setUsernameAttribute() {
-        $this->attributes['username'] = str_slug($this->username, "-");
+        $this->attributes['username'] = Str::slug($this->username, "-");
     }
 
     public function scopeAdmin($query)
